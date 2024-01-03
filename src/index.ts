bd66e7d0ca0fedd55c934899d5e8bc86801836b1
TheRedMine TheredMaths <theredminedu51@gmail.com>
2024-01-03 23:10:45 +0100

    init

---
+++ src/index.ts
@@ -0,0 +1,90 @@
+import axios from 'axios';
+import { config } from 'dotenv';
+import { connect } from 'mongoose';
+
+import MovieModel from './model';
+
+// @ts-ignore
+const data = require('../data.json') as typeof import('../data.json');
+// @ts-ignore
+const genres = require('../genres.json') as typeof import('../genres.json');
+
+interface SearchResults {
+    page: number;
+    results: {
+        adult: boolean;
+        backdrop_path: string;
+        genre_ids: number[],
+        id: number;
+        original_language: string;
+        original_title: string;
+        overview: string;
+        poster_path: string;
+        release_date: string;
+        title: string;
+        video: boolean;
+        vote_average: number;
+        vote_count: number;
+    }[];
+    total_pages: number;
+    total_results: number;
+}
+
+// populate `process.env`
+config();
+
+// define new API instance
+const api = axios.create({
+    baseURL: "https://api.themoviedb.org/3/",
+    headers: {
+        Authorization: `Bearer ${process.env.AUTH_TOKEN}`,
+    },
+});
+
+const reqApi = (name: string) => api.get<SearchResults>('/search/movie?query=' + name);
+
+// @ts-ignore // mongoose connect
+connect(process.env.MONGO_URI!, {}).then(() => {
+    data.forEach(async (e, key) => { try {
+        const res = await reqApi(e.name);
+        const {
+            title,
+            adult,
+            genre_ids,
+            id,
+            backdrop_path,
+            original_language,
+            original_title,
+            overview,
+            poster_path,
+            release_date,
+        } = res.data.results[0];
+
+        // get genres
+        const filmGenres = genre_ids.map(g => (
+            genres.find(v => v.id === g)?.name || "0"
+        )).filter(e => e !== "0");
+
+        await MovieModel.create({
+            title,
+            link: e.uqload,
+            backdrop_path,
+            poster_path,
+            description: overview,
+            categories: filmGenres,
+            actors: e.actors.split(', '),
+            nsfw: adult,
+            release_date,
+            original_title,
+            original_language,
+            tmdb_id: id,
+            author: e.author,
+            time: e.time,
+        });
+
+        await new Promise((resolve) => setTimeout(resolve, 200));
+        console.log(key);
+    } catch (err) {
+        console.error("An error has occured:", e.name);
+    }});
+});
---
+++ src/model.ts
@@ -0,0 +1,22 @@
+import { Schema, model } from 'mongoose';
+
+const MovieSchema = new Schema({
+    title: { type: String, required: true },
+    link: { type: String, required: true },
+    backdrop_path: { type: String, required: true },
+    poster_path: { type: String, required: true },
+    description: { type: String, required: true },
+    categories: { type: [String], required: true },
+    actors: { type: [String], required: true },
+    addedAt: { type: Date, default: new Date() },
+    nsfw: { type: Boolean, required: true },
+    release_date: { type: String, required: true },
+    original_title: { type: String, required: true },
+    original_language: { type: String, required: true },
+    tmdb_id: { type: Number, required: true },
+    author: { type: String, required: true },
+    time: { type: String, required: true },
+});
+
+const MovieModel = model('movies', MovieSchema);
+export default MovieModel;
